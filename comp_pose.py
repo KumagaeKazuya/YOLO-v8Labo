19069f644d8e79f3858614bb2ff72be12171ef4c
KumagaeKazuya <2211170075k@ed.fuk.kindai.ac.jp>
2025-07-03 16:11:31 +0900

    メインループの処理を分割

--- comp_pose.py
+++ comp_pose.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import time
 import math
+import gdown
 from collections import defaultdict, deque
 import logging
 
@@ -361,18 +362,3 @@ class DrowsinessDetectionSystem:
         cap.release()
         cv2.destroyAllWindows()
 
-
-def main():
-    rtsp_url = "rtsp://6199:4003@192.168.100.183/live"
-    model_path = "yolov8m-pose.pt"
-    detector = DrowsinessDetectionSystem(rtsp_url, model_path)
-    video_path = "627lab1.mp4"  # 動画ファイルのパス
-    save_path = "output.mp4"
-    log_path = "frame_results.csv"
-
-    detector = DrowsinessDetectionSystem("", model_path)
-    detector.run_on_video(video_path, save_path, log_path)
-
-
-if __name__ == "__main__":
-    main()
---
+++ downloader.py
@@ -0,0 +1,22 @@
+import gdown
+import os
+
+def download_file_from_google_drive(file_id: str, save_dir="input_videos", file_name=None):
+    """
+    Google Driveからファイルをダウンロードする関数。
+    :param file_id: Google DriveのファイルID
+    :param save_dir: 保存先ディレクトリ
+    :param file_name: 保存するファイル名。Noneの場合は元のファイル名を使用
+    :return: ダウンロードしたファイルのパス
+    """
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    if file_name is None:
+        file_name = f"{file_id}.mp4"
+
+    file_path = os.path.join(save_dir, file_name)
+
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(f"https://drive.google.com/uc?id={file_id}", file_path, quiet=False)
+    return file_path
